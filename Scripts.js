97959ca4c4e99c3311d0390fbff6ef86511a693a
Daniel Vera <d.vera.acero@gmail.com>
2022-08-31 23:12:38 -0500

    Script with issues solved

--- Scripts.js
+++ Scripts.js
@@ -18,15 +18,15 @@ btnScissor.addEventListener('click',playerChooseScissor)
 
 function playerChooseRock() {
     optionPlayer = 1;
-    screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/zXTLCKGQ/Rock-Bot.png)'
+    screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/DZwcY8Y2/Rock.png)'
 }
 function playerChoosePaper() {
     optionPlayer = 2;
-    screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/WpBFqXTr/Paper-Bot.png)'
+    screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/4ytwpjSt/paper.png)'
 }
 function playerChooseScissor() {
     optionPlayer = 3;
-    screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/J7DHh9VM/Scissors-Bot.png)'
+    screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/Ss1G0kwc/Scissors.png)'
 }
 
 function getRandomIntInclusive(min, max) {
@@ -34,9 +34,8 @@ function getRandomIntInclusive(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
+/* 
 function compareResult(choosePlayer,chooseAI) {
-    btnPlay.innerHTML = "Play again!"
     if ((choosePlayer == 1 && chooseAI == 3) || (choosePlayer == 2 && chooseAI == 1) || (choosePlayer == 3 && chooseAI == 2)) {
         optionPlayer = 0;
         countWin = countWin + 1;
@@ -60,6 +59,45 @@ function compareResult(choosePlayer,chooseAI) {
     } else {
         alert('Please select an option');
     }
+    btnPlay.innerHTML = "Play again!"
+}
+ */
+
+function compareResult(choosePlayer,chooseAI) {
+    if (btnPlay.innerHTML == "Play again!") {
+        btnPlay.innerHTML = "Play"
+        screenResultPlayer.classList.add('inactive');
+        screenResultAI.classList.add('inactive');
+        textResult.classList.add('inactive');
+    } else {
+        if ((choosePlayer == 1 && chooseAI == 3) || (choosePlayer == 2 && chooseAI == 1) || (choosePlayer == 3 && chooseAI == 2)) {
+            optionPlayer = 0;
+            countWin = countWin + 1;
+            scorePlayer.innerHTML = "Score: " + countWin
+            screenResultPlayer.classList.remove('inactive');
+            screenResultAI.classList.remove('inactive');
+            textResult.classList.remove('inactive');
+            textResult.innerHTML = 'YOU WON!'
+        } else if ((choosePlayer == 1 && chooseAI == 2) || (choosePlayer == 2 && chooseAI == 3) || (choosePlayer == 3 && chooseAI == 1)) {
+            optionPlayer = 0;
+            countLose = countLose + 1;
+            scoreAI.innerHTML = "Score: " + countLose;
+            screenResultPlayer.classList.remove('inactive');
+            screenResultAI.classList.remove('inactive');
+            textResult.classList.remove('inactive');
+            textResult.innerHTML = 'AI WON, BETTER LUCK NEXT TIME!'
+        } else if ((choosePlayer == 1 && chooseAI == 1) || (choosePlayer == 2 && chooseAI == 2) || (choosePlayer == 3 && chooseAI == 3)) {
+            optionPlayer = 0;
+            btnPlay.innerHTML = "Draw! Try again";
+            screenResultPlayer.classList.remove('inactive');
+            screenResultAI.classList.remove('inactive');
+            textResult.classList.remove('inactive');
+            textResult.innerHTML = 'DRAW!'
+        } else {
+            alert('Please select an option');
+        }
+        btnPlay.innerHTML = "Play again!"
+    }
 }
 
 function rockPaperScissors(event) {
@@ -67,13 +105,13 @@ function rockPaperScissors(event) {
     let optionAI = getRandomIntInclusive(1,3)
     if (optionAI == 1) {
         optionAI = 1
-        screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/zXTLCKGQ/Rock-Bot.png)'
+        screenResultAI.style.backgroundImage='url(https://i.postimg.cc/zXTLCKGQ/Rock-Bot.png)'
     } else if (optionAI == 2) {
         optionAI = 2
-        screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/WpBFqXTr/Paper-Bot.png)'
+        screenResultAI.style.backgroundImage='url(https://i.postimg.cc/WpBFqXTr/Paper-Bot.png)'
     } else if (optionAI == 3) {
         optionAI = 3
-        screenResultPlayer.style.backgroundImage='url(https://i.postimg.cc/J7DHh9VM/Scissors-Bot.png)'
+        screenResultAI.style.backgroundImage='url(https://i.postimg.cc/J7DHh9VM/Scissors-Bot.png)'
     }
     compareResult(optionPlayer,optionAI)
 }
